bd8c995d2c2f8ebbae2ec7b7545357ef857bfbf1
JonathanWillnow <jona.willnow@hotmail.de>
2023-05-31 21:30:13 +0200

    first experiments with sizing on ipad and iphone

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,15 @@
 import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import ThemeContext from "../context/ThemeContext";
 import Overview from "./Overview";
 import Details from "./Details";
 import IS from "./IncomeStatement";
 import FD from "./FactorData";
-import CF from "./CFStatement"
+import CF from "./CFStatement";
 import Chart from "./Chart";
 import Header from "./Header";
 import StockContext from "../context/StockContext";
-import { fetchStockDetails, fetchQuote, fetchIncomeStatementData , fetchfactordata, fetchCFStatementData} from "../utils/api/stock-api";
+import { fetchStockDetails, fetchQuote, fetchIncomeStatementData, fetchfactordata, fetchCFStatementData } from "../utils/api/stock-api";
 
 const Dashboard = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -23,14 +24,14 @@ const Dashboard = () => {
   const [factordata, setfactordata] = useState({});
 
   const [quote, setQuote] = useState({});
-  
+
   useEffect(() => {
-    
+
     const updateIS = async () => {
       try {
         const result = await fetchIncomeStatementData(stockSymbol);
         console.log(result);
-    
+
         const formattedData = Object.values(result).map((item) => ({
           label: item.year.toString(),
           data: [
@@ -47,12 +48,12 @@ const Dashboard = () => {
         console.log(error);
       }
     };
-    
+
     const updateCF = async () => {
       try {
         const result = await fetchCFStatementData(stockSymbol);
         console.log(result);
-    
+
         const formattedData = Object.values(result).map((item) => ({
           label: item.year.toString(),
           data: [
@@ -69,12 +70,12 @@ const Dashboard = () => {
         console.log(error);
       }
     };
-    
+
 
     const updatefactordata = async () => {
       try {
         const result = await fetchfactordata(stockSymbol);
-        console.log(result) 
+        console.log(result)
         setfactordata(result);
       } catch (error) {
         setfactordata({});
@@ -86,7 +87,7 @@ const Dashboard = () => {
       try {
         const result = await fetchStockDetails(stockSymbol);
         setStockDetails(result);
-        // 
+        //
       } catch (error) {
         setStockDetails({});
         console.log(error);
@@ -109,18 +110,19 @@ const Dashboard = () => {
     updateStockDetails();
     updateStockOverview();
   }, [stockSymbol]);
+
   return (
-    <div
-      className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-12 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${
-        darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"
-      }`}
-    >
+    <div className={`grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-12 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand ${darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100"}`}>
       <div className="col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center">
         <Header name={stockDetails.name} />
       </div>
-      <div className="md:col-span-2 row-span-4">
-        <Chart />
-      </div>
+      {!window.matchMedia("(max-width: 600px)").matches && ( // Hide the chart section for screens smaller than 600px width
+        <div className="md:col-span-2 row-span-4">
+          <div className="w-full h-full md:h-full">
+            <Chart />
+          </div>
+        </div>
+      )}
       <div>
         <Overview
           symbol={stockSymbol}
@@ -134,13 +136,13 @@ const Dashboard = () => {
         <Details details={stockDetails} />
       </div>
       <div className="col-span-1 row-span-4">
-       <FD details={factordata} />
+        <FD details={factordata} />
       </div>
-      <div className="col-span-2 row-span-4">
-       <IS details={incomeStatement} />
+      <div className="col-span-2 md:col-span-2 row-span-4">
+        <IS details={incomeStatement} />
       </div>
-      <div className="col-span-2 row-span-4">
-       <CF details={CFStatement} />
+      <div className="col-span-2 md:col-span-2 row-span-4">
+        <CF details={CFStatement} />
       </div>
     </div>
   );
